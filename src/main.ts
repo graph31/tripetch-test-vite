8d0e6e7c9f1bd01d8a1e7648dce9bda84a4b73ac
Warithar K <warithar.dev@outlook.co.th>
2023-11-02 00:52:33 +0700

    Add: Icon.

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,9 @@ import App from './App.vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 
-/* add icons to the library */
-library.add(faUserSecret)
-
+library.add(fas, fab)
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
